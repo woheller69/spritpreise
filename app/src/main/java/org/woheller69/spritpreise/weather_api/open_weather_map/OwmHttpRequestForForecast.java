e327fb8d96d52686e49acb08f42c942b311ceb74
woheller69 <wolfgang.heller@web.de>
2022-03-29 11:27:29 +0200

    remove OwmHttpRequest.java

--- app/src/main/java/org/woheller69/spritpreise/weather_api/open_weather_map/OwmHttpRequestForForecast.java
+++ app/src/main/java/org/woheller69/spritpreise/weather_api/open_weather_map/OwmHttpRequestForForecast.java
@@ -2,16 +2,20 @@ package org.woheller69.spritpreise.weather_api.open_weather_map;
 
 import android.content.Context;
 
+import androidx.preference.PreferenceManager;
+
+import org.woheller69.spritpreise.BuildConfig;
 import org.woheller69.spritpreise.http.HttpRequestType;
 import org.woheller69.spritpreise.http.IHttpRequest;
 import org.woheller69.spritpreise.http.VolleyHttpRequest;
+import org.woheller69.spritpreise.preferences.AppPreferencesManager;
 import org.woheller69.spritpreise.weather_api.IHttpRequestForForecast;
 
 /**
  * This class provides the functionality for making and processing HTTP requests to the
  * OpenWeatherMap to retrieve the latest weather data for all stored cities.
  */
-public class OwmHttpRequestForForecast extends OwmHttpRequest implements IHttpRequestForForecast {
+public class OwmHttpRequestForForecast  implements IHttpRequestForForecast {
 
     /**
      * Member variables.
@@ -33,5 +37,17 @@ public class OwmHttpRequestForForecast extends OwmHttpRequest implements IHttpRe
         IHttpRequest httpRequest = new VolleyHttpRequest(context);
         final String URL = getUrlForQueryingForecast(context, lat, lon);
         httpRequest.make(URL, HttpRequestType.GET, new ProcessOwmForecastRequest(context));
+    }
+
+    protected String getUrlForQueryingForecast(Context context, float lat, float lon) {
+        AppPreferencesManager prefManager =
+                new AppPreferencesManager(PreferenceManager.getDefaultSharedPreferences(context));
+        return String.format(
+                "%sforecast?lat=%s&lon=%s&units=metric&appid=%s",
+                BuildConfig.BASE_URL,
+                lat,
+                lon,
+                prefManager.getOWMApiKey(context)
+        );
     }
 }
