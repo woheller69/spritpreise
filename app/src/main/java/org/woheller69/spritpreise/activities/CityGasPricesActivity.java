b3b97533e18fbaaacdfd88155c3eb6b9151f544a
woheller69 <wolfgang.heller@web.de>
2022-09-26 18:10:36 +0200

    Location button: add empty city in no location is defined

--- app/src/main/java/org/woheller69/spritpreise/activities/CityGasPricesActivity.java
+++ app/src/main/java/org/woheller69/spritpreise/activities/CityGasPricesActivity.java
@@ -92,8 +92,10 @@ public class CityGasPricesActivity extends NavigationActivity implements IUpdate
             long updateInterval = (long) (Float.parseFloat(prefManager.getString("pref_updateInterval", "15")) * 60);
 
             if (timestamp + updateInterval - systemTime <= 0) {
-                CityPagerAdapter.refreshSingleData(getApplicationContext(),true, cityId); //only update current tab at start
-                CityGasPricesActivity.startRefreshAnimation();
+                if (cityId!=getWidgetCityID(context)||locationListenerGPS==null) { //do not update first TAB while location is updating
+                    CityPagerAdapter.refreshSingleData(getApplicationContext(), true, cityId); //only update current tab at start
+                    CityGasPricesActivity.startRefreshAnimation();
+                }
             }
             if (viewPager2.getCurrentItem()!=pagerAdapter.getPosForCityID(cityId)) viewPager2.setCurrentItem(pagerAdapter.getPosForCityID(cityId),false);
         }
@@ -124,8 +126,10 @@ public class CityGasPricesActivity extends NavigationActivity implements IUpdate
                 long updateInterval = (long) (Float.parseFloat(prefManager.getString("pref_updateInterval", "15")) * 60);
 
                 if (timestamp + updateInterval - systemTime <= 0) {
-                    CityPagerAdapter.refreshSingleData(getApplicationContext(),true, pagerAdapter.getCityIDForPos(position));
-                    CityGasPricesActivity.startRefreshAnimation();
+                    if (pagerAdapter.getCityIDForPos(position)!=getWidgetCityID(context)||locationListenerGPS==null) { //do not update first TAB while location is updating
+                        CityPagerAdapter.refreshSingleData(getApplicationContext(),true, pagerAdapter.getCityIDForPos(position));
+                        CityGasPricesActivity.startRefreshAnimation();
+                    }
                 }
 
                 cityId=pagerAdapter.getCityIDForPos(viewPager2.getCurrentItem());  //save current cityId for next resume
@@ -166,22 +170,23 @@ public class CityGasPricesActivity extends NavigationActivity implements IUpdate
         SharedPreferences prefManager = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
         updateLocationButton = menu.findItem(R.id.menu_update_location);
         SQLiteHelper db = SQLiteHelper.getInstance(this);
-        if(prefManager.getBoolean("pref_GPS", true)==TRUE && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED && !db.getAllCitiesToWatch().isEmpty()) {
+        if(prefManager.getBoolean("pref_GPS", true)==TRUE && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED ) {
             updateLocationButton.setVisible(true);
             updateLocationButton.setActionView(R.layout.menu_update_location_view);
             updateLocationButton.getActionView().clearAnimation();
-            if (locationListenerGPS!=null) {  //GPS still trying to get new location
-                if (updateLocationButton != null && updateLocationButton.getActionView() != null) {
-                    startUpdateLocatationAnimation();
+            if (locationListenerGPS!=null) {  //GPS still trying to get new location -> stop and restart to get around problem with tablayout not updating
+                removeLocationListener();
+                if (!db.getAllCitiesToWatch().isEmpty()) {  //if city has not been removed continue location update
+                    locationListenerGPS=getNewLocationListener();
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListenerGPS);
+                    if (updateLocationButton != null && updateLocationButton.getActionView() != null) {
+                        startUpdateLocatationAnimation();
+                    }
                 }
             }
             updateLocationButton.getActionView().setOnClickListener(v -> m.performIdentifierAction(updateLocationButton.getItemId(), 0));
         }else{
-            if (locationListenerGPS!=null) {
-                locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-                if (locationListenerGPS!=null) locationManager.removeUpdates(locationListenerGPS);
-            }
-            locationListenerGPS=null;
+            removeLocationListener();
             if (updateLocationButton != null && updateLocationButton.getActionView() != null) {
                 updateLocationButton.getActionView().clearAnimation();
             }
@@ -207,50 +212,23 @@ public class CityGasPricesActivity extends NavigationActivity implements IUpdate
                 CityGasPricesActivity.startRefreshAnimation();
             }
         }else if (id==R.id.menu_update_location) {
-            if (!db.getAllCitiesToWatch().isEmpty()) {  //only if at least one city is watched, otherwise crash
-                locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
-                SharedPreferences prefManager = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
-                if (prefManager.getBoolean("pref_GPS", true) == TRUE && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
-                    if (locationListenerGPS == null) {
-                        Log.d("GPS", "Listener null");
-                        locationListenerGPS = new LocationListener() {
-                            @Override
-                            public void onLocationChanged(android.location.Location location) {
-                                Log.d("GPS", "Location changed");
-                                SQLiteHelper db = SQLiteHelper.getInstance(context);
-                                CityToWatch city = db.getCityToWatch(getWidgetCityID(context));
-                                    city.setLatitude((float) location.getLatitude());
-                                    city.setLongitude((float) location.getLongitude());
-                                    city.setCityName(String.format(Locale.getDefault(), "%.2f° / %.2f°", location.getLatitude(), location.getLongitude()));
-                                    db.updateCityToWatch(city);
-                                    db.deleteStationsByCityId(getWidgetCityID(context));
-                                    pagerAdapter.loadCities();
-                                    viewPager2.setAdapter(pagerAdapter);
-                                    tabLayout.getTabAt(0).setText(city.getCityName());
-                                    if (locationListenerGPS!=null) locationManager.removeUpdates(locationListenerGPS);
-                                    locationListenerGPS=null;
-                                if (updateLocationButton != null && updateLocationButton.getActionView() != null) {
-                                    updateLocationButton.getActionView().clearAnimation();
-                                }
-                            }
-
-                            @Deprecated
-                            @Override
-                            public void onStatusChanged(String provider, int status, Bundle extras) {
-                            }
-
-                            @Override
-                            public void onProviderEnabled(String provider) {
-                            }
-
-                            @Override
-                            public void onProviderDisabled(String provider) {
-                            }
-                        };
-                        Log.d("GPS", "Request Updates");
-                        CityGasPricesActivity.startUpdateLocatationAnimation();
-                        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListenerGPS);
-                    }
+            if (db.getAllCitiesToWatch().isEmpty())  {
+                CityToWatch newCity = new CityToWatch(db.getMaxRank() + 1, -1, -1, 0, 0, "--°/--°");
+                cityId = (int) db.addCityToWatch(newCity);
+                initResources();
+                noCityText.setVisibility(View.GONE);
+                viewPager2.setVisibility(View.VISIBLE);
+                viewPager2.setAdapter(pagerAdapter);
+                TabLayoutMediator tabLayoutMediator = new TabLayoutMediator(tabLayout, viewPager2,false,false, (tab, position) -> tab.setText(pagerAdapter.getPageTitle(position)));
+                tabLayoutMediator.attach();
+            }
+            locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+            SharedPreferences prefManager = PreferenceManager.getDefaultSharedPreferences(getApplicationContext());
+            if (prefManager.getBoolean("pref_GPS", true) == TRUE && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                if (locationListenerGPS == null) {
+                    locationListenerGPS = getNewLocationListener();
+                    CityGasPricesActivity.startUpdateLocatationAnimation();
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1000, 0, locationListenerGPS);
                 }
             }
         }
@@ -333,6 +311,50 @@ public class CityGasPricesActivity extends NavigationActivity implements IUpdate
                 updateLocationButton.getActionView().startAnimation(blink);
             }
         }
+    }
+
+    private LocationListener getNewLocationListener() {
+        return new LocationListener() {
+            @Override
+            public void onLocationChanged(android.location.Location location) {
+                Log.d("GPS", "Location changed");
+                SQLiteHelper db = SQLiteHelper.getInstance(context);
+                CityToWatch city = db.getCityToWatch(getWidgetCityID(context));
+                city.setLatitude((float) location.getLatitude());
+                city.setLongitude((float) location.getLongitude());
+                city.setCityName(String.format(Locale.getDefault(), "%.2f° / %.2f°", location.getLatitude(), location.getLongitude()));
+                db.updateCityToWatch(city);
+                db.deleteStationsByCityId(getWidgetCityID(context));
+                pagerAdapter.loadCities();
+                viewPager2.setAdapter(pagerAdapter);
+                tabLayout.getTabAt(0).setText(city.getCityName());
+                removeLocationListener();
+                if (updateLocationButton != null && updateLocationButton.getActionView() != null) {
+                    updateLocationButton.getActionView().clearAnimation();
+                }
+            }
+
+            @Deprecated
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+            }
+        };
+    }
+
+    private void removeLocationListener() {
+        if (locationListenerGPS!=null) {
+            locationManager = (LocationManager) context.getSystemService(Context.LOCATION_SERVICE);
+            if (locationListenerGPS!=null) locationManager.removeUpdates(locationListenerGPS);
+        }
+        locationListenerGPS=null;
     }
 }
 
